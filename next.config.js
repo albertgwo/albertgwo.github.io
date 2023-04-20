5652999680b8a6a9e389ba8e33282da478a290f6
Albert Gwo <albert@enclave.market>
2023-04-19 22:35:43 -0700

    try this

--- next.config.js
+++ next.config.js
@@ -12,14 +12,14 @@ const nextConfig = {
     dirs: ['src'],
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.svg$/i,
+  //     issuer: /\.[jt]sx?$/,
+  //     use: ['@svgr/webpack'],
+  //   });
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
