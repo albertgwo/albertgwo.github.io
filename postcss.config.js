64f37d4a3c27c17f46c79fed30d86dbe5cbfb93a
Albert Gwo <albert@enclave.market>
2023-04-19 22:40:53 -0700

    try this

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-opacity': {
+          '--text-opacity': '1',
+        },
+        '.text-no-opacity': {
+          '--text-opacity': '0',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+    require('autoprefixer'),
+  ],
 };
