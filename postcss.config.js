24bb27d557fddff2294ec48fa7d99a5ca68c9377
Albert Gwo <albert@enclave.market>
2023-04-19 23:03:49 -0700

    more cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,3 @@
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    plugin(function ({ addUtilities }) {
-      const newUtilities = {
-        '.text-opacity': {
-          '--text-opacity': '1',
-        },
-        '.text-no-opacity': {
-          '--text-opacity': '0',
-        },
-      };
-
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    }),
-    require('autoprefixer'),
-  ],
+  plugins: ['tailwindcss'],
 };
